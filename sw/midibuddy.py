10795baaa92d7b416cde226f97749f6a678cf331
Beat Besmer <beat.besmer@gmail.com>
2025-04-01 21:55:24 +0200

    Adapt for ESP32 S3

--- sw/midibuddy.py
+++ sw/midibuddy.py
@@ -1,30 +1,41 @@
-import machine
-uart = machine.UART(0,31250)
-
-
-print("Detaching REPL (use webrepl)")
-import os
-os.dupterm(None, 1)
+from machine import UART, Pin
 import time
+import binascii
+import struct
 
-# MIDI IN
-while True:
-    if (uart.any()):
-        print(uart.read())
+midi_baud = 31250
+tx = 36
+rx = 35
+uart=UART(1, baudrate=midi_baud, bits=8, tx=tx, rx=rx, invert=UART.INV_RX)
 
+# Midi IN (weird bytes being received?)
+# Middle C pressed as hard as possible and then released
+# 28 2 28 254
+# Middle C Pressed softly
+# 28 250 28 254 
+# Middle C pressed quickly
+# 28 74 152 
 
+while False:
+    if uart.any():
+        msg = uart.read(2)
+        m = struct.unpack('B', msg)[0]
+        if m != 0:
+            #print(int.from_bytes(msg), end=' ')
+            #print(binascii.hexlify(msg).decode(), end='.')
+            print(m,end=' ')
 
 
 # MIDI OUT
 notes = [60,61,62,63,64,63,62,61]
-import ustruct
-import time
+delay = 0.5
 
 while True:
   for x in notes:
-    print("note")
-    uart.write(ustruct.pack("bbb",0x90,x,127))
-    time.sleep(0.5)
-    time.sleep(0.5)
-    uart.write(ustruct.pack("bbb",0x80,x,0))
-    
+    print(f"Sending Midi note #{x}...", end= '')
+    uart.write(struct.pack("bbb",0x90,x,77))
+    time.sleep(delay)
+    uart.write(struct.pack("bbb",0x80,x,0))
+    uart.write(struct.pack("bbb",0x80,x,0))
+    print(" Note off")
+    time.sleep(delay)
